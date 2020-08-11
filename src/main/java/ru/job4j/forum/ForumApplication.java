7af70030a1ab651259f04a44eeb35f6b3ee28afc
Valentin Kalinin <k-valentin-1989@yandex.ru>
2020-08-11 19:54:04 +0300

    add componentscan

--- src/main/java/ru/job4j/forum/ForumApplication.java
+++ src/main/java/ru/job4j/forum/ForumApplication.java
@@ -7,10 +7,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 
 import javax.sql.DataSource;
 
 @SpringBootApplication
+@ComponentScan({"ru.job4j.forum.*"})
 public class ForumApplication extends SpringBootServletInitializer {
 
     @Override
